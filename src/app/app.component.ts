c699ba1c95157ac64200774f1616f5236677417f
Sanel Hadzini <sanel.hadzini@yahoo.com>
2021-10-19 20:46:43 +0200

    generate password on button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,8 @@ export class AppComponent {
   providedLength = 0;
   useLetters = false;
   useNumbers = false;
-  useSymbols = true;
+  useSymbols = false;
   password = '';
-
-  onButtonClick() {
-    this.password = '';
-  }
 
   getPassword(): string {
     return this.password;
@@ -25,11 +21,11 @@ export class AppComponent {
   }
 
   onChangeUseNumbers(): void {
-    this.useLetters = !this.useLetters;
+    this.useNumbers = !this.useNumbers;
   }
 
   onChangeUseSymbols(): void {
-    this.useLetters = !this.useLetters;
+    this.useSymbols = !this.useSymbols;
   }
 
   onChangeLength(value: string): void {
@@ -38,5 +34,39 @@ export class AppComponent {
     if (isNaN(providedLength)) return;
 
     this.providedLength = providedLength;
+  }
+
+  private getValidPasswordCharacters = (): string => {
+    let validCharacters = '';
+
+    const availableCharacterSets = {
+      numbers: '1234567890',
+      letters: 'abcdefghijklmnopqrstuvwxyz',
+      symbols: '!@#$%^&*&(',
+    };
+
+    if (this.useLetters)
+      validCharacters = validCharacters.concat(availableCharacterSets.letters);
+    if (this.useNumbers)
+      validCharacters = validCharacters.concat(availableCharacterSets.numbers);
+    if (this.useSymbols)
+      validCharacters = validCharacters.concat(availableCharacterSets.symbols);
+
+    const password = Array(this.providedLength)
+      .fill(null)
+      .map(() => {
+        const randomlyPickedItem =
+          validCharacters[Math.floor(Math.random() * validCharacters.length)];
+        return randomlyPickedItem;
+      })
+      .join('');
+
+    console.log(password);
+
+    return password;
+  };
+
+  onButtonClick() {
+    this.password = this.getValidPasswordCharacters();
   }
 }
