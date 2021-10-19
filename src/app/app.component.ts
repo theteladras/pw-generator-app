dda480792b19ee9bbe58c047f5121549be65493a
Sanel Hadzini <sanel.hadzini@yahoo.com>
2021-10-19 20:01:35 +0200

    handle checkboxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw-generator';
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = true;
+  password = '';
+
+  onButtonClick() {
+    this.password = '';
+  }
+
+  getPassword(): string {
+    return this.password;
+  }
+
+  onChangeUseLetters(): void {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseNumbers(): void {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseSymbols(): void {
+    this.useLetters = !this.useLetters;
+  }
 }
